6651270dfdb207225dc126eb4601e197a91f296c
dharmesh soni <dharmeshsoni999@gmail.com>
2022-12-28 21:16:59 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+
+import Signup from "./components/Signup/Signup";
+import Signin from "./components/Signin/Signin";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import './App.css';
+import PropertyView from './components/PropertyView/PropertyView';
+import Logout from "./components/Logout/Logout";
+import AddProperty from "./components/AddProperty/addProperty";
+
+
+function App() {
+  return (
+    <div className="App">
+   
+      <BrowserRouter>
+      <Routes>
+            <Route path="/" element={<Signin/>}></Route>
+             <Route path="/signup" element={<Signup/>}></Route> 
+            <Route path="/listproperty" element={<PropertyView/>}></Route>
+            <Route path="/addproperty" element={<AddProperty/>}></Route>
+            <Route path="/logout"  element={<Logout/>}></Route> 
+          </Routes>
+          
+      </BrowserRouter>
+     
+    </div>
+
+  );
+}
+
+export default App;
+
+
+ // "start": "react-scripts start",
+    // "build": "react-scripts build",
+    // "test": "react-scripts test",
+    // "eject": "react-scripts eject"
